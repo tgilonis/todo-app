71a429e23bd50cc4f171e239ecb1a69c11b72a33
tgilonis <tgilonis@protonmail.com>
2021-12-04 13:25:28 +0000

    Converted fields to controlled fields

--- src/components/todo/LoginComponent.jsx
+++ src/components/todo/LoginComponent.jsx
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
 
 class LoginComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: 'DefaultValue',
+            password: ''
+        }
+        this.handleUserNameChange = this.handleUserNameChange.bind(this)
+        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    }
+
     render() { 
         return (
             <div>
-            User Name: <input type="text" name="username"/>
-            Password: <input type="password" name="password"/>
+            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUserNameChange}/>
+            Password: <input type="password" name="password" value={this.state.password} onChange = {this.handlePasswordChange}/>
             <button>Login</button>
             </div>
         )
+    }
+
+    handleUserNameChange(event) {
+        // controlled component - the input's value is always driven by the React state
+        // advantage of this is the ability to pass the value to other UI elements
+        this.setState({username : event.target.value})
+    }
+
+    handlePasswordChange(event) {
+        this.setState({password : event.target.value})
     }
 }
 

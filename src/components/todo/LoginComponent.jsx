5e9334a9a38e2969147442be924dabcdc45adb09
tgilonis <tgilonis@protonmail.com>
2021-12-04 13:31:48 +0000

    Refactored handleChange to allow common events

--- src/components/todo/LoginComponent.jsx
+++ src/components/todo/LoginComponent.jsx
@@ -8,28 +8,28 @@ class LoginComponent extends Component {
             username: 'DefaultValue',
             password: ''
         }
-        this.handleUserNameChange = this.handleUserNameChange.bind(this)
-        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        
     }
 
     render() { 
         return (
             <div>
-            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUserNameChange}/>
-            Password: <input type="password" name="password" value={this.state.password} onChange = {this.handlePasswordChange}/>
+            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+            Password: <input type="password" name="password" value={this.state.password} onChange = {this.handleChange}/>
             <button>Login</button>
             </div>
         )
     }
 
-    handleUserNameChange(event) {
+    handleChange(event) {
         // controlled component - the input's value is always driven by the React state
         // advantage of this is the ability to pass the value to other UI elements
-        this.setState({username : event.target.value})
-    }
-
-    handlePasswordChange(event) {
-        this.setState({password : event.target.value})
+        this.setState(
+            {
+                [event.target.name] : event.target.value
+            }
+        )
     }
 }
 

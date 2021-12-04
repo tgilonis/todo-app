f8cc7f5309b91171c052609654ed664df91aab00
tgilonis <tgilonis@protonmail.com>
2021-12-04 13:50:09 +0000

    Added conditional function components

--- src/components/todo/LoginComponent.jsx
+++ src/components/todo/LoginComponent.jsx
@@ -6,20 +6,35 @@ class LoginComponent extends Component {
         super(props)
         this.state = {
             username: 'DefaultValue',
-            password: ''
+            password: '',
+            hasLoginFailed: null
         }
         this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
         
     }
 
     render() { 
         return (
-            <div>
-            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
-            Password: <input type="password" name="password" value={this.state.password} onChange = {this.handleChange}/>
-            <button>Login</button>
+            <div>               
+                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>
+                <ShowLoginSuccessful hasLoginFailed={this.state.hasLoginFailed}/>
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+                Password: <input type="password" name="password" value={this.state.password} onChange = {this.handleChange}/>
+                <button onClick={this.loginClicked}>Login</button>
             </div>
         )
+    }
+
+    
+    loginClicked(event) {
+        if(this.state.username==='theo' && this.state.password==='dummy') {
+            this.setState({showSuccessMessage: true})
+            this.setState({hasLoginFailed: false})
+        } else {
+            this.setState({showSuccessMessage: false})
+            this.setState({hasLoginFailed: true})
+        }
     }
 
     handleChange(event) {
@@ -33,5 +48,21 @@ class LoginComponent extends Component {
         )
     }
 }
+
+//function components
+function ShowInvalidCredentials(props) {
+    if(props.hasLoginFailed) {
+        return <div>Invalid Credentials</div>
+    }
+    return null;
+}
+
+function ShowLoginSuccessful(props) {
+    if(props.hasLoginFailed === false) {
+        return <div>Login Successful!</div>
+    }
+    return null;
+}
+
 
 export default LoginComponent;

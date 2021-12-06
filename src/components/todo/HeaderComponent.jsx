992afb9dcbbe9fd20a2c29c7eeca2f610463eb96
tgilonis <tgilonis@protonmail.com>
2021-12-06 18:39:27 +0000

    Added links to header

--- src/components/todo/HeaderComponent.jsx
+++ src/components/todo/HeaderComponent.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class HeaderComponent extends Component {
     render() {
@@ -7,12 +8,12 @@ class HeaderComponent extends Component {
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                     <div><a href="http://localhost:3000/login" className="navbar-brand">Todos</a></div>
                     <ul className="navbar-nav">
-                        <li className="nav-link">Home</li>
-                        <li className="nav-link">Todos</li>
+                        <li><Link className="nav-link" to="/welcome/theo">Home</Link></li>
+                        <li><Link className="nav-link" to="/todos">Todos</Link></li>
                     </ul>
                     <ul className="navbar-nav navbar-collapse justify-content-end">
-                        <li className="nav-link">Login</li>
-                        <li className="nav-link">Logout</li>
+                        <li><Link className="nav-link" to="/login">Login</Link></li>
+                        <li><Link className="nav-link" to="/logout">Logout</Link></li>
                     </ul>
                 </nav>
             </header>

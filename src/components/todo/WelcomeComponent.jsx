11fa542c2b2a26269f769c8ce45c02279981b2d9
tgilonis <tgilonis@protonmail.com>
2021-12-06 18:50:55 +0000

    Formatted welcome component

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -3,9 +3,14 @@ import {Link} from 'react-router-dom'
 
 class WelcomeComponent extends Component {
     render() {
-        return <div>
-            Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link>.
-            </div>
+        return (
+            <>
+                <h1>Welcome!</h1>
+                <div className="container">
+                    Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link>.
+                </div>
+            </>
+        )
     }
 }
 

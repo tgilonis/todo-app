010925e23fe95efcb472ca0bee29560ed8f0a052
tgilonis <tgilonis@protonmail.com>
2021-12-10 17:48:59 +0000

    Added basic button to welcome for api testing

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
 
 class WelcomeComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+    }
+
     render() {
         return (
             <>
                 <h1>Welcome!</h1>
                 <div className="container">
-                    Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link>.
+                    Welcome {this.props.match.params.name}. 
+                    You can manage your todos <Link to="/todos">here</Link>.
+                </div>
+                <div className="container">
+                    Click here to get a customized welcom message.
+                    <button onClick={this.retrieveWelcomeMessage} 
+                    className="btn btn-success">Get Welcome Message</button>
                 </div>
             </>
         )
+    }
+    retrieveWelcomeMessage() {
+        console.log("Welcome");
     }
 }
 

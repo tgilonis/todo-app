37c7f254f2c3d32fa97186d477e505e7829c04a1
tgilonis <tgilonis@protonmail.com>
2021-12-10 18:19:43 +0000

    Now displaying response from server on welcome screen

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
+import HelloWorldService from '../../api/todo/HelloWorldService';
 
 class WelcomeComponent extends Component {
 
     constructor(props) {
         super(props)
         this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
+        this.state = {
+            welcomeMessage : ''
+        }
     }
 
     render() {
@@ -21,11 +26,20 @@ class WelcomeComponent extends Component {
                     <button onClick={this.retrieveWelcomeMessage} 
                     className="btn btn-success">Get Welcome Message</button>
                 </div>
+                <div className="container">
+                    {this.state.welcomeMessage}
+                </div>
             </>
         )
     }
     retrieveWelcomeMessage() {
-        console.log("Welcome");
+        HelloWorldService.executeHelloWorldService()
+        .then(response => this.handleSuccessfulResponse(response))
+        .catch(console.log("Failure"))
+    }
+
+    handleSuccessfulResponse(response) {
+        this.setState({welcomeMessage : response.data})
     }
 }
 

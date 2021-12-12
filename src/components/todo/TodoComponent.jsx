77545091ddc504eefa5a6656c97cec0048d10734
tgilonis <tgilonis@protonmail.com>
2021-12-12 16:55:00 +0000

    Added basic todo form layout

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,9 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import moment from 'moment'
+import { Formik, Form, Field } from 'formik'
 
 class TodoComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            id : 1,
+            description : 'Learn Forms',
+            date : moment(new Date()).format('YYYY-MM-DD')            
+        }
+    }
+
     render() {
-        return <div>Todo Component for id - {this.props.match.params.id}</div>
-        
+        return (
+        <div>
+            <h1>Todo</h1>
+            <div className="container">
+                <Formik>
+                    {
+                        (props) => (
+                            <Form>
+                                <fieldset className="form-group">
+                                    <label>Description</label>
+                                    <Field className="form-control" type="text" name="description"></Field>
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Target Date</label>
+                                    <Field className="form-control" type="date" name="targetDate"></Field>
+                                </fieldset>
+                                <button className="btn btn-success" type="submit">Save</button>
+                            </Form>                            
+                        )                        
+                    }
+                </Formik>
+            </div>
+        </div>
+        )        
     }
 }
 

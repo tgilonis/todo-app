ee77bf85339898297a7320d41de45038e7ba1c8f
tgilonis <tgilonis@protonmail.com>
2021-12-14 19:42:45 +0000

    Added basic validation

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import moment from 'moment'
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 class TodoComponent extends Component {
 
@@ -8,20 +8,59 @@ class TodoComponent extends Component {
         super(props)
         this.state = {
             id : 1,
-            description : 'Learn Forms',
+            description : 'Learn Forms Now',
             date : moment(new Date()).format('YYYY-MM-DD')            
         }
+        this.onSubmit = this.onSubmit.bind(this)
+        this.validate = this.validate.bind(this)
+    }
+
+    onSubmit(values) {
+
+    }
+
+    validate(values) {
+        let errors = {}
+
+        if(!values.description) {
+            errors.description = "Enter a description."
+        } else if (values.description.length<5) {
+            errors.description = "Enter at least 5 characters in the description."
+        }
+
+        if(!moment(values.targetDate).isValid()) {
+            errors.targetDate = "Enter a valid date."
+        }
+
+        return errors
     }
 
     render() {
+        //destructure the state variables
+        let { description, targetDate } = this.state
+        // let description = this.state.description
+        // let targetDate = this.state.targetDate
+        
+
         return (
         <div>
             <h1>Todo</h1>
             <div className="container">
-                <Formik>
+                <Formik
+                    initialValues={{
+                        // equivalent of description : description etc.
+                        description,
+                        targetDate
+                    }}
+                    onSubmit={this.onSubmit}
+                    validateOnChange={false}
+                    validateOnBlur={false}
+                    validate={this.validate}>
                     {
                         (props) => (
                             <Form>
+                                <ErrorMessage name="description" component="div" className="alert alert-warning"></ErrorMessage>
+                                <ErrorMessage name="targetDate" component="div" className="alert alert-warning"></ErrorMessage>
                                 <fieldset className="form-group">
                                     <label>Description</label>
                                     <Field className="form-control" type="text" name="description"></Field>
